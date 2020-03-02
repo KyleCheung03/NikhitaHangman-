d4b4c7d751379c7c7d537fe067fc34d5c8b0caa3
KyleCheung03 <60932839+KyleCheung03@users.noreply.github.com>
2020-03-02 11:01:14 -0500

    added to Nikhita's file

--- hangman B.py
+++ hangman B.py
@@ -1,38 +1,79 @@
-''' Initialize global variables'''
+import random
 word = ""
 letter = ""
-updatedSpaces = []
-
-def intialize():
-    ''' List = ["Dog","Cat","Tree"]
-    random.choice(
-    '''
-    print("print word")
+updatedspaces = []
+def intialize ():
+    wordBank = ['keyboard', 'computer', 'mouse', 'application']
+    word = random.choice(wordBank)
+    updatedspaces = []
+    spaces = "___"
+    print(spaces), len(word)
     
-
 def getLetter():
-    '''Out letter from user'''
+    letter = raw_input('Guess a letter ')
     global letter
+    letterList = []
+    global letterList  
+    letterList.append(letter)
     
-def checkLetter():
-    '''tests if letter in secret, 
-    if True updates undatedSpaces displays 
-        updatedSoaces, calls checkIfWon
-    if False displays updatedSpaces, updates lives, calls 
-        getLetter
-    '''
-    global updatedSpaces
-    
-def checkIfWon():
-    '''outputs a message if won or 
-        calls getLetter if not'''
-    print()
 
-def main():
-    '''Starts the program'''
+def checkifWon ():
+    if len(letterList) == len(word): 
+        print('Good job, you won!')
+        
+    else:
+        getLetter()
+            
+
+def main ():
     intialize()
     getLetter()
-    checkLetter()
+    checkifWon()
 
 main()
+
+main()
+word = ("Anir")
+wordList = list(word.lower())
+updatedSpaces = []
+wordLen = len(word)
+lives = 5
+letter = " "
+
+for i in range (0, int(wordLen)):
+    updatedSpaces.append("_")
     
+def getLetter():
+    global letter
+    letter = raw_input ("Enter a letter guess    ")
+
+def check():
+    global updatedSpaces
+    global lives
+    global letter
+    for x in range(0, int(wordLen)):
+        if letter == wordList[x]:
+            updatedSpaces[x] = wordList[x]
+            print(updatedSpaces)
+            print("You have:   ", lives, "   lives left!")
+            checklist = "".join(updatedSpaces)
+            master = "".join(wordList)
+            if checklist == master:
+                print("Congrats you solved the word!    ")
+                #break
+            else:
+                getLetter()
+        else:
+            lives -= 1
+            if lives != 0:
+                print("You have:  " + str(lives) + "  lives left!")
+                print(updatedSpaces)
+                getLetter()
+            else:
+                print("Game Over   ")
+                
+def game():
+    getLetter()
+    check()
+    
+game()
